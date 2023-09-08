c9d63cde8dc6b1bd012d6076fb8921f298994225
tal098123 <62802353+tal098123@users.noreply.github.com>
2023-09-08 20:47:21 +0300

    yes

--- vm-webapp-flask-script.sh
+++ vm-webapp-flask-script.sh
@@ -13,8 +13,14 @@ Flask Flask-SQLAlchemy psycopg2
 apt-get install -y python3-flask python3-itsdangerous python3-pyinotify python3-simplejson python3-werkzeug
 git clone https://github.com/tal098123/baruchi_project.git
 # Run the Flask app
-python3 /var/lib/waagent/custom-script/download/0/tr_progect/flask_app/app.py
+# current_dir="$( cd "$(dirname "$0")" ; pwd -P )"
+# Get the latest script directory
+latest_dir=$(ls -v /var/lib/waagent/custom-script/download/ | tail -n 1)
 
+python3 /var/lib/waagent/custom-script/download/$latest_dir/baruchi_project/flask_app/app.py &
+# python3 /var/lib/waagent/custom-script/download/0/baruchi_project/flask_app/app.py &
+
+# python3 /home/tal/baruchi_project/flask_app/app.py
 
 
 
